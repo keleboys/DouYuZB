09bc882c67f183c0fac19aaca31fa0efd56d9445
zq <13253467705@163.com>
2021-12-17 15:26:57 +0800

    Revert "test"
    This reverts commit b9736150da239f587739f8c965eb65ad4a8e01a7.

--- DouYuZB/Calsses/Profile/Controller/ZQProfileViewController.swift
+++ DouYuZB/Calsses/Profile/Controller/ZQProfileViewController.swift
@@ -11,7 +11,7 @@ class ZQProfileViewController: ZQBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "李雷"
+        self.title = ""
         // Do any additional setup after loading the view.
     }
 }
